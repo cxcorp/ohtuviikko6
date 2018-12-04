71c2d525b7414d55aa49a0b716c5326f9b776e91
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-12-04 22:51:34 +0200

    Refactor QueryBuilder

--- QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
+++ QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -16,26 +16,32 @@ public class QueryBuilder {
     }
 
     public QueryBuilder playsIn(String team) {
-        return new QueryBuilder(
-            new And(matcher, new PlaysIn(team))
+        return withNewAndMatcher(
+            new PlaysIn(team)
         );
     }
 
     public QueryBuilder hasAtLeast(int value, String category) {
-        return new QueryBuilder(
-            new And(matcher, new HasAtLeast(value, category))
+        return withNewAndMatcher(
+            new HasAtLeast(value, category)
         );
     }
 
     public QueryBuilder hasFewerThan(int value, String category) {
-        return new QueryBuilder(
-            new And(matcher, new HasFewerThan(value, category))
+        return withNewAndMatcher(
+            new HasFewerThan(value, category)
         );
     }
 
     public QueryBuilder oneOf(Matcher... matchers) {
         return new QueryBuilder(
             new Or(matchers)
+        );
+    }
+
+    private QueryBuilder withNewAndMatcher(Matcher newMatcher) {
+        return new QueryBuilder(
+            new And(matcher, newMatcher)
         );
     }
 }
