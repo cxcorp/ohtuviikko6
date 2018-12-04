5654bdb0f87ffd847e169ce421fbb18beab59671
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-12-04 20:11:38 +0200

    Finish exercise 5

--- QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
+++ QueryLanguage/src/main/java/statistics/matcher/QueryBuilder.java
@@ -1,24 +1,41 @@
 package statistics.matcher;
 
 public class QueryBuilder {
-    private Matcher matcher = new All();
+    private Matcher matcher;
+
+    public QueryBuilder() {
+        this(new All());
+    }
+
+    private QueryBuilder(Matcher matcher) {
+        this.matcher = matcher;
+    }
 
     public Matcher build() {
         return matcher;
     }
 
     public QueryBuilder playsIn(String team) {
-        matcher = new And(matcher, new PlaysIn(team));
-        return this;
+        return new QueryBuilder(
+            new And(matcher, new PlaysIn(team))
+        );
     }
 
     public QueryBuilder hasAtLeast(int value, String category) {
-        matcher = new And(matcher, new HasAtLeast(value, category));
-        return this;
+        return new QueryBuilder(
+            new And(matcher, new HasAtLeast(value, category))
+        );
     }
 
     public QueryBuilder hasFewerThan(int value, String category) {
-        matcher = new And(matcher, new HasFewerThan(value, category));
-        return this;
+        return new QueryBuilder(
+            new And(matcher, new HasFewerThan(value, category))
+        );
+    }
+
+    public QueryBuilder oneOf(Matcher... matchers) {
+        return new QueryBuilder(
+            new Or(matchers)
+        );
     }
 }
